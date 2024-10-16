ce6f1857cb1a74288ace7b57cb6203c1a0569697
Sroopesh1122 <shivuroopesh6362@gmail.com>
2024-10-16 13:31:12 +0530

    search suggestions added

--- src/pages/seeker/ListOfCompanies.jsx
+++ src/pages/seeker/ListOfCompanies.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from "react";
 import MainContext from "../../components/MainContext";
-import { AutoComplete, ConfigProvider, Pagination, Skeleton } from "antd";
+import { AutoComplete, ConfigProvider, Pagination, Skeleton, Spin } from "antd";
 import { AuthContext } from "../../contexts/AuthContext";
 import { axiosInstance } from "../../utils/axiosInstance";
 import { useQuery } from "@tanstack/react-query";
@@ -13,6 +13,10 @@ const ListOfCompanies = () => {
   const [totalData, setTotalData] = useState(pageSize || 10);
   const [currentPage, setCurrentPage] = useState(1);
   const [searchText, setSearchText] = useState("");
+  const [searchData, setSearchData] = useState([]);
+  const [searchLoading, setSearchLoading] = useState(false);
+  const [openSuggestion,setSuggestionOpen] =useState(false)
+
 
   const handlePageChange = (val) => {
     setCurrentPage(val);
@@ -32,7 +36,7 @@ const ListOfCompanies = () => {
     return res.data.pageData;
   };
 
-  const { data, isLoading, isFetching, isError, error } = useQuery({
+  const { data, isLoading, isFetching, isError, error, refetch } = useQuery({
     queryKey: ["search-comapny", currentPage],
     queryFn: () => fetchCompanies(currentPage),
     keepPreviousData: true,
@@ -40,7 +44,55 @@ const ListOfCompanies = () => {
     cacheTime: 10000,
   });
 
-  console.log(data);
+  useEffect(() => {
+    const fetchSearchData = async () => {
+      if (searchText === "") {
+        setSearchData([]);
+        return;
+      }
+      const controller = new AbortController();
+      const signal = controller.signal;
+      try {
+        setSearchLoading(true);
+
+        const res = await axiosInstance.get(
+          `/provider/searchCompany/${searchText}`,
+          {
+            signal,
+          }
+        );
+
+        if (res.data) {
+          const searchedData = res.data.map((sdata) => ({
+            value: sdata.company_name,
+          }));
+          setSearchData(searchedData);
+        }
+      } catch (error) {
+        if (axios.isCancel(error)) {
+          console.log("Request canceled", error.message);
+        } else {
+          console.error("Error fetching search data:", error);
+        }
+      } finally {
+        setSearchLoading(false);
+      }
+
+      return () => {
+        controller.abort(); // Cleanup to cancel any ongoing requests
+      };
+    };
+
+    const delayDebounceFn = setTimeout(() => {
+      fetchSearchData();
+    }, 500);
+
+    return () => clearTimeout(delayDebounceFn);
+  }, [searchText]);
+
+  const handleSearchTextChange = (e) => {
+    setSearchText(e);
+  };
 
   return (
     <MainContext>
@@ -57,25 +109,39 @@ const ListOfCompanies = () => {
               }}
             >
               <AutoComplete
-                options={[]}
+                options={searchData}
                 className="rounded-lg auto-complete-custom !w-[250px] "
                 style={{
                   border: "1px solid black ",
                 }}
+                open={openSuggestion}
+                onFocus={()=>setSuggestionOpen(true)}
+                value={searchText}
+                onChange={handleSearchTextChange}
                 placeholder="Search Company By Name"
+                notFoundContent={searchLoading ? <Spin size="small" /> : null}
+                onSelect={(e) => setSearchText(e)}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter") {
+                    refetch();
+                    setSuggestionOpen(false)
+                  }
+                }}
               />
             </ConfigProvider>
             <button
               type="button"
               className="p-1 px-2 bg-orange-600 ms-2 rounded-full text-sm"
+              onClick={() => {refetch();setSuggestionOpen(false)}}
             >
               Search
             </button>
           </div>
-
           <div className="mt-10 w-full">
             <h1>Search Results:</h1>
-            <div className="mt-2 grid grid-cols-1 sm:grid-cols-2 gap-5 sm:p-5  ">
+            <div
+              className={"mt-2  grid grid-cols-1 sm:grid-cols-2 gap-5 sm:p-5 "}
+            >
               {isLoading || isFetching ? (
                 [...Array(pageSize)].map((i) => <CompanySearchCardSkeleton />)
               ) : (
@@ -140,7 +206,7 @@ const CompanySearchCard = ({ data }) => {
   const [userId, setUserId] = useState("");
   const { profileData } = useContext(AuthContext);
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   useEffect(() => {
     setUserId(profileData?.user_id);
@@ -155,8 +221,10 @@ const CompanySearchCard = ({ data }) => {
     company_id,
   } = data;
   return (
-    <div className="w-full h-[130px] md:h-[150px] rounded-lg flex justify-between items-center cursor-pointer border border-gray-300 p-4 primary-shadow-hover relative hover:scale-[1.0002] "
-    onClick={()=>navigate(`/user/company/${company_id}`)}>
+    <div
+      className="w-full h-[130px] md:h-[150px] rounded-lg flex justify-between items-center cursor-pointer border border-gray-300 p-4 primary-shadow-hover relative hover:scale-[1.0002] "
+      onClick={() => navigate(`/user/company/${company_id}`)}
+    >
       {followers?.find((uid) => uid === userId) ? (
         <div className="absolute top-2 right-2">
           <CustomBadge text="Following" bgcolor="#1E5BF0" text_color="white" />
@@ -171,7 +239,8 @@ const CompanySearchCard = ({ data }) => {
         <h6 className="text-[0.8rem] font-extralight mb-0">
           No of Post :{" "}
           {job_details.jobs
-            ? job_details?.jobs?.length + project_details?.projects?.length
+            ? job_details?.jobs?.length +
+              (project_details?.projects?.length || 0)
             : 0}
         </h6>
         <h6 className="text-[0.8rem] font-extralight mb-0">
