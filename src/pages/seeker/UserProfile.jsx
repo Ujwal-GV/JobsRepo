c9ba952a785e4770eb4a170a669c8b294e11300c
Ujwal Gowda V <ujwalgowda2002v@gmail.com>
2024-12-08 18:51:25 +0530

    Candidate card adjusted

--- src/pages/seeker/UserProfile.jsx
+++ src/pages/seeker/UserProfile.jsx
@@ -672,6 +672,7 @@ const UserProfile = () => {
             <AnimateEnterExit transition={{ duration: 0.2 }} position="!fixed">
               <ProfilePersonalDetailsModal
                 open={personalDetailsModalOpen}
+                saveLoading={profilePersonalDetailsUpdateMutation.isPending}
                 onClose={() => {
                   setPersonalDetailsModalOpen(false);
                   freeBody();
@@ -1293,7 +1294,10 @@ const ProfilePersonalDetailsModal = ({
     mobile: null,
     gender: null,
   },
+  saveLoading = false,
 }) => {
+  const [loading, setLoading] = useState(false);
+
   return (
     <div
       className={
@@ -1417,9 +1421,9 @@ const ProfilePersonalDetailsModal = ({
               <div className="w-full mt-4 center gap-3">
                 <button
                   type="submit"
-                  className="btn-orange px-3 border py-1 border-transparent tracking-widest"
+                  className="btn-orange px-3 border py-1 border-transparent tracking-widest flex center gap-1"
                 >
-                  Save
+                  {saveLoading && <LuLoader2 className="animate-spin-slow " />} Save
                 </button>
                 <button
                   type="button"
