8bd8dc84bb7aec2e32de8fbe6526e352dc46c194
Sroopesh1122 <shivuroopesh6362@gmail.com>
2024-12-19 19:40:12 +0530

    blocked login added

--- src/pages/freelancer/queries/FreelancerProfileQuery.jsx
+++ src/pages/freelancer/queries/FreelancerProfileQuery.jsx
@@ -2,10 +2,14 @@ import { useQuery } from "@tanstack/react-query";
 import { axiosInstance } from "../../../utils/axiosInstance";
 import { useContext } from "react";
 import { AuthContext } from "../../../contexts/AuthContext";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 export const useGetFreelancerProfileData = ()=>{
 
   const {userRole , setProfileData} = useContext(AuthContext);
+
+    const navigate = useNavigate();
 
   const getFreelancerProfileData = async()=>{
       const res = await axiosInstance.get("/freelancer/profile");
@@ -13,6 +17,13 @@ export const useGetFreelancerProfileData = ()=>{
       {
         setProfileData(res.data);
       }
+      if(res.data.isBlocked)
+        {
+          toast.error("Your Account is blocked.Please contact support team");
+          localStorage.removeItem('authToken')
+          sessionStorage.removeItem('location');
+          navigate("/login");
+        }
       return res.data;
     }
 
